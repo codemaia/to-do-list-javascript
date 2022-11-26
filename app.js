21617c3e8ed1f3e9fcedf0e26bfeb2fd8c5b4172
Renan Maia <renanmaia.rl@gmail.com>
2022-11-25 23:17:28 -0300

    creating remove to-do

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
+
 const formAddTodo = document.querySelector('.form-add-todo');
+const todosContainer = document.querySelector('.todos-container');
 
 
+
+//add to-do
 formAddTodo.addEventListener('submit', event => {
     event.preventDefault();
 
-    const inputValue = event.target.add.value;
-    console.log(inputValue);
+    const inputValue = event.target.add.value.trim();
+
+    if (inputValue) {
+        todosContainer.innerHTML += `
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span>${inputValue}</span>
+                <i class="far fa-trash-alt delete"></i>
+            </li>
+        `
+
+        event.target.reset();
+    }
     
+});
+
+
+
+//remove to-do
+todosContainer.addEventListener('click', event => {
+    const clickedElement = event.target;
+    
+    if (Array.from(clickedElement.classList).includes('delete')) {
+        clickedElement.parentElement.remove();
+    };
 
 });
+
+
+
